9af8a127efadc66bcb3f7b94f7cf1b412e877b7b
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-09-16 01:42:42 -0400

    chore: remove heartbeat logging as it is redundant

--- app/utilities/reporting.py
+++ app/utilities/reporting.py
@@ -7,7 +7,6 @@ from app.utilities.logging import logger
 # Send a heartbeat the the uptime monitor
 async def ping_uptime_monitor():
     while True:
-        logger.debug("Sending heartbeat to uptime monitor.")
         try:
             requests.get(UPTIME_MONITOR)
         except Exception as e:
